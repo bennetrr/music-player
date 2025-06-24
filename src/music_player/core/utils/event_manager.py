2515cd493ef6eb2b20674ec185077149774179ef
Bennet Ranft <76742318+bennetrr@users.noreply.github.com>
2025-06-24 18:44:15 +0200

    chore: Refactor EventManager

--- src/music_player/core/utils/event_manager.py
+++ src/music_player/core/utils/event_manager.py
@@ -3,10 +3,10 @@ __all__ = ['EventManager']
 from collections.abc import Callable
 
 
-class EventManager[TEvent]:
+class EventManager[TPayload]:
     """Class for managing event listeners."""
 
-    type TListener = Callable[[TEvent], None]  # type: ignore[valid-type]
+    type TListener = Callable[[TPayload], None]  # type: ignore[valid-type]
     _listeners: list[TListener]
 
     def __init__(self) -> None:
@@ -18,12 +18,12 @@ class EventManager[TEvent]:
         if listener not in self._listeners:
             self._listeners.append(listener)
 
-    def remove(self, listener: TListener) -> None:
+    def unlisten(self, listener: TListener) -> None:
         """Remove a listener from the event manager."""
         if listener in self._listeners:
             self._listeners.remove(listener)
 
-    def invoke(self, event: TEvent) -> None:
-        """Invoke all listeners with the given event."""
+    def invoke(self, payload: TPayload) -> None:
+        """Invoke all listeners with the given payload."""
         for listener in self._listeners:
-            listener(event)
+            listener(payload)
